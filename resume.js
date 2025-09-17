3820b2946da21b97435d0a92322b88ecbe9c7355
tessamhutch-web <tessamhutch@gmail.com>
2025-09-17 10:18:34 -0400

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,13 @@
+let myName = "Tessa";
+let title = "Student";
+let education ="Oral Roberts University";
+let experience = "Crumbl, Cinnabon, Coffee Shop";
+let skills =["Baking", "Scuba diving", "Communicator"];
+
+
+//Connect Variables to the HTML (ligaments)
+document.getElementById("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
+document.getElementById("skills").textContent = skills;
