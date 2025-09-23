496d0e47e3b5163432d28fefc451a4d7d530c5a3
tessamhutch-web <tessamhutch@gmail.com>
2025-09-23 16:22:26 -0400

    Add files via upload

--- resume.js
+++ resume.js
@@ -1,13 +1,30 @@
+
+
 let myName = "Tessa";
+console.log("Hello my name is Tessa");
+let myEmail = "hutch4371@oru.edu";
 let title = "Student";
 let education ="Oral Roberts University";
 let experience = "Crumbl, Cinnabon, Coffee Shop";
-let skills =["Baking", "Scuba diving", "Communicator"];
+let skills = ["Baking", "Leadership", "Communication"];
+let skillsList = document.createElement("ul")
 
 
-//Connect Variables to the HTML (ligaments)
 document.getElementById("my-name").textContent = myName;
+document.getElementById("my-email").textContent = myEmail;
 document.getElementById("title").textContent = title;
 document.getElementById("experience").textContent = experience;
 document.getElementById("education").textContent = education;
-document.getElementById("skills").textContent = skills;
+document.getElementById("skills-section").appendChild(skillsList);
+
+for (let i = 0; i < skills.length; i++) {
+ let li = document.createElement("li");
+ li.textContent = skills[i];
+ skillsList.appendChild(li);
+}
+
+
+function showFunFact() {
+ alert("Fun Fact: I love scuba diving!");
+}
+
